c8a32200836f87e394b30b46fa4f44af72a3f516
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-21 15:46:34 +0900

    prob3

--- src/prob03/CurrencyConverter.java
+++ src/prob03/CurrencyConverter.java
@@ -4,12 +4,15 @@ public class CurrencyConverter {
 	private static double rate;
 
 	public static void setRate(double rate) {
+		CurrencyConverter.rate=rate;
 	}
 	
 	public static double toDollar( double krw ) {
+		return krw/rate;
 	}
 	
 	public static double toKRW( double dollar ) {
+		return dollar*rate;
 	}
 	
 }
--- src/prob03/CurrencyConverterTest.java
+++ src/prob03/CurrencyConverterTest.java
@@ -4,6 +4,16 @@ public class CurrencyConverterTest {
 
 	public static void main(String[] args) {
 		//  환율을 세팅 합니다.
+		CurrencyConverter.setRate(1076.20);
+		
+		//백만원 환전
+		double dollar=CurrencyConverter.toDollar(1000000);
+		System.out.println("백만원은 "+dollar+"달러입니다.");
+		
+		//백달러 환전
+		double krw=CurrencyConverter.toKRW(100);
+		System.out.println("백달러는 "+krw+"원입니다.");
+		
 	}
 
 }
