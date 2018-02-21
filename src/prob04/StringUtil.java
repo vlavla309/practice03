2e19c0a4359d44bc7f38893b12aad716b24f64bc
BIT <BIT@DESKTOP-RS6IVTV>
2018-02-21 15:48:59 +0900

    prob4

--- src/prob04/StringUtil.java
+++ src/prob04/StringUtil.java
@@ -2,6 +2,11 @@ package prob04;
 
 public class StringUtil {
 	public static String concatenate( String[] str ) {
+		String result= new String();
+		for (String string : str) {
+			result+=string;
+		}
+		return result;
 	}
 	
 }
